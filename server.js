e6c2437ab33b9593957c4b2e0faab53dc1707383
fabioalxk <fabioalxk@users.noreply.github.com>
2019-08-14 17:07:13 -0300

    its working route

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ sequelize
 app.use("/api/items", require("./routes/api/Item"));
 app.use("/api/users", require("./routes/api/users"));
 
+app.get("*", (req, res) => {
+  res.send("It's working");
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
