b5f258c7c77df1472b0f1c3390af454fee6f9e06
fabioalxk <fabioalxk@users.noreply.github.com>
2019-08-15 09:31:29 -0300

    postman scripts sent

--- config/database.js
+++ config/database.js
@@ -1,28 +1,28 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  "d25chigea5a8dg",
-  "ubiuhcvctxayta",
-  "10812456038dc74898663d2b31ef0b445358c0ff9f3f3fae143681f1fcc18387",
-  {
-    host: "ec2-50-19-109-120.compute-1.amazonaws.com",
-    dialect: "postgres",
-    protocol: "postgres",
-    dialectOptions: {
-      ssl: true
-    }
-  }
-);
-
-// const sequelize = new Sequelize("test", "postgres", "postgres", {
-//   host: "localhost",
-//   dialect: "postgres",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
+// const sequelize = new Sequelize(
+//   "d25chigea5a8dg",
+//   "ubiuhcvctxayta",
+//   "10812456038dc74898663d2b31ef0b445358c0ff9f3f3fae143681f1fcc18387",
+//   {
+//     host: "ec2-50-19-109-120.compute-1.amazonaws.com",
+//     dialect: "postgres",
+//     protocol: "postgres",
+//     dialectOptions: {
+//       ssl: true
+//     }
 //   }
-// });
+// );
+
+const sequelize = new Sequelize("test", "postgres", "postgres", {
+  host: "localhost",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
 
 module.exports = sequelize;
